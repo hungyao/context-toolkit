2ff43a9d86353332d64b2377374c9525bc27c6e8
beone.brian <beone.brian@dd1d7481-0460-7213-5eef-3d21d792f96f>
2010-12-29 08:39:28 +0000

    git-svn-id: http://contexttoolkit.googlecode.com/svn/trunk@31 dd1d7481-0460-7213-5eef-3d21d792f96f

--- src/context/apps/demos/IntelligibilityDemos.java
+++ src/context/apps/demos/IntelligibilityDemos.java
@@ -1,15 +1,15 @@
 package context.apps.demos;
 
-import context.apps.ContextModel;
 import context.apps.demos.accelerometer.AccelerometerApplication;
 import context.apps.demos.homeactivity.HomeApplication;
-import context.apps.demos.imautostatus.AutostatusApplication;
+import context.apps.demos.imautostatus.AutostatusIntelligibleApplication;
 import context.apps.demos.roomlight.RoomApplication;
+import context.arch.discoverer.Discoverer;
 
 public class IntelligibilityDemos {
 	
 	public static void main(String[] args) {
-		ContextModel.startDiscoverer();
+		Discoverer.start();
 		
 		/*
 		 * Start multiple apps
@@ -21,8 +21,7 @@ public class IntelligibilityDemos {
 		AccelerometerApplication accelDemo = new AccelerometerApplication();
 		accelDemo.setVisible(true);
 
-		AutostatusApplication autostatusDemo = new AutostatusApplication("Bob");
-		autostatusDemo.setVisible(true);
+		new AutostatusIntelligibleApplication("Bob");
 
 		HomeApplication homeDemo = new HomeApplication();
 		homeDemo.setVisible(true);
